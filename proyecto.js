2fcd8d2a526ea689101527bda940d90ba013a172
DariusCorrea <dariocorreadc428@gmail.com>
2022-09-07 00:09:07 -0300

    se agrega proyecto

---
+++ proyecto.js
@@ -0,0 +1,27 @@
+//Calculadora de IMC
+
+let pesoUsuario
+let esturaUsuario
+let imcUsuario
+let salir
+do {
+pesoUsuario = parseFloat (prompt ("¿Cual es tu peso en KG?"));
+    esturaUsuario = parseFloat ( prompt ("¿Cual es tu estatura en metros cuadrados?"));
+imcUsuario = pesoUsuario / (esturaUsuario * esturaUsuario);
+alert ("Tu IMC es "+imcUsuario);
+if (imcUsuario < 18.5) {
+    alert ("Tu peso es delgado, deberás subir de peso.");
+
+}
+
+else if ((imcUsuario >= 18.5)  && (imcUsuario <= 24.99) ) {
+    alert ("Tu peso es normal, mantente así.");
+}
+else if ((imcUsuario >= 25) && (imcUsuario < 30)) {
+    alert ("Estás en sobrepeso, deberás bajar de peso.");
+}
+else if (imcUsuario >= 30) {
+    alert ("Estás en estado de obesidad, debes bajar tu peso, ve al medico!");
+}
+salir = prompt ("Si deseas calcular otro IMC usa s, si deseas salir usa n");
+} while (salir != "n")
