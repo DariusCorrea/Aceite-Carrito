867e29561b7fb9364535cde613dec3419293a05d
DariusCorrea <dariocorreadc428@gmail.com>
2022-09-15 19:07:26 -0300

    proyecto carrito de compras

--- proyecto.js
+++ proyecto.js
@@ -13,7 +13,7 @@ do {
 pesoUsuario = parseFloat (prompt ("¿Cual es tu peso en KG?"));
 peso();
 function peso() {
-let edad= prompt ("Coloque su edad: ")
+let edad= prompt ("Coloque su edad: ");
 
 }
 
@@ -37,3 +37,67 @@ else if (imcUsuario >= 30) {
 salir = prompt ("Si deseas calcular otro IMC usa s, si deseas salir usa n");
 } while (salir != "n")
 
+/* Proyecto carrito de compras */
+
+  class Producto{
+  constructor (nombre, precio){
+      this.nombre=nombre;
+      this.precio=precio;
+  
+  }
+  }
+  
+  const aceite = new Producto ("Aceite", 1700);
+  
+  const oliva = new Producto ("Aceite de oliva", 2500);
+  
+  const arrayProductos = [aceite , oliva];
+  
+  const contenedor= document.getElementById("contenedor");
+  
+  arrayProductos.forEach (Producto =>{
+  let div = document.createElement("div");
+  div.innerHTML = `    <div class="producto">
+  <div class="producto-blanco">
+    <img class="edit" src="./resources/olive.jpg">
+  </div>
+
+  <div class="producto-informacion">
+  <p class="producto-nombre">Aceite Natura</p>
+  <p class="producto-precio">1800$</p>
+  <button class="editar__boton">Agregar al carrito</button>
+  <button class="editar__boton">Quitar del carrito</button>
+        </div>
+
+</div>
+<div class="producto">
+  <div class="producto-blanco">
+    <img class="edit" src="./resources/cocina.jpg">
+  </div>
+
+  <div class="producto-informacion">
+  <p class="producto-nombre">Aceite Natura</p>
+  <p class="producto-precio">1800$</p>
+  <button class="editar__boton">Agregar al carrito</button>
+  <button class="editar__boton">Quitar del carrito</button>
+        </div>
+
+</div>
+<div class="producto">
+  <div class="producto-blanco">
+    <img class="edit" src="./resources/cocina.jpg">
+  </div>
+
+  <div class="producto-informacion">
+  <p class="producto-nombre">Aceite Natura</p>
+  <p class="producto-precio">1800$</p>
+  <button class="editar__boton">Agregar al carrito</button>
+  <button class="editar__boton">Quitar del carrito</button>
+        </div>
+
+</div>`;
+  contenedor.appendChild(div);
+  })  
+  
+  
+  
