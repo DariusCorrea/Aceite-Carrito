5804d1c5dfd1aa0d59021624fe3722ddc5f934c2
DariusCorrea <dariocorreadc428@gmail.com>
2022-09-20 16:09:13 -0300

    se agregaron imagenes

--- datos.js
+++ datos.js
@@ -1,10 +1,10 @@
 
 const Productos = [
-    {id:"1", Producto: "aceite de oliva", precio: 2400, imagen: "../resources/litro.webp"},
+    {id:"1", Producto: "Aceite Natura", precio: 2400, imagen: "../resources/litro.webp"},
     {id:"2", Producto: "aceite de cocina", precio: 1700, imagen: "../resources/natura.png"},
     {id:"3", Producto: "aceite de oliva ", precio: 1100, imagen: "../resources/cocinero.webp"},
-    {id:"4", Producto: "aceite cañuelas 1l", precio: 1300, imagen: "../resources/cañuelas.webp"},
-    {id:"5", Producto: "aceite buena calidad", precio: 1200, imagen: "../resources/olive.jpg"},
+    {id:"4", Producto: "Aceite cañuelas 1L", precio: 1300, imagen: "../resources/cañuelas.png"},
+    {id:"5", Producto: "Aceite cañuelas 5L", precio: 1200, imagen: "../resources/5litros.jpg"},
     {id:"6", Producto: "aceite natura", precio: 1900, imagen: "../resources/natura-oliva.jpg"},
     {id:"7", Producto: "aceite cañuelas oliva", precio: 2600, imagen: "../resources/olive-cañuelas.jpg"},
     {id:"8", Producto: "aceite cañuelas girasol", precio: 3000, imagen: "../resources/cinco.jpg"},
--- proyecto.js
+++ proyecto.js
@@ -1,3 +1,4 @@
+
 let carrito= []
 const contenedor= document.getElementById("contenedor");
 console.log(console);
@@ -11,19 +12,29 @@ card.innerHTML=`
   <div class="card-body">
     <h5 class="card-title">${element.Producto}</h5>
     <p class="card-text">$${element.precio}</p>
-  <button id="agregar${element.id}" class="btn btn-secundary editar__boton d-flex">Agregar al carrito</button> 
+  <button id="agregar${element.id}" class="btn btn-dark ">Agregar al carrito</button>
+
 </div>
 </div>`
 contenedor.appendChild(card)
+
 let botonAgregar=document.getElementById(`agregar${element.id}`);
 botonAgregar.addEventListener("click", () =>{
-  console.log(`${element.Producto}`)
+  console.log(`${element.Producto}`) 
   alert("Su producto fue agregado con éxito "+ element.precio)
+
 })
   });
+  
 }
-const agregarProducto=(prodId) => {
-  const item = contenedor.find((prod) => prod.id === prodId)
+
+
+function agregarProductos(element){
+  console.log(`${element.Producto}`)
+  alert(element.precio)
+}
+const agregarProducto=(element) => {
+  const item = contenedor.find((Producto) => element.id === element.id)
   carrito.push(item)
   console.log(carrito)
 }
